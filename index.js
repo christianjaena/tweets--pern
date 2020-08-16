88767a7adb73a9fd7c96b8bf321feb364cff7e1a
Christian Jaena <christian.jaena@msugensan.edu.ph>
2020-08-16 20:23:27 +0800

    fixed for deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const PORT = process.env.PORT || 5000; 
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build/'));
+	app.use(express.static('client/build'));
 }
  else {
 	const morgan = require('morgan');
